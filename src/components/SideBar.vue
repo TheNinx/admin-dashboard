d04c5a92e946be217d80952d70409ebf4299fbdb
wingles <wingles@girolando.com.br>
2022-05-25 13:23:03 -0300

    add SideBar component

---
+++ src/components/SideBar.vue
@@ -0,0 +1,29 @@
+<template>
+  <header>
+  <h1><img alt="Vue logo" src="../assets/logo.png"></h1>-->
+  </header>
+
+
+
+</template>
+
+<script>
+
+import {defineComponent} from "vue";
+
+export default  defineComponent({
+  name: "SideBar"
+})
+</script>
+
+<style scoped>
+
+header {
+  padding: 1rem;
+  background: #0d3b66;
+  width: 100%;
+  height: 100vh;
+  text-align: center;
+}
+
+</style>
